956464f5a20e311d6804e7c94f5e09bd4210fa41
leong_jia_wei <wwwbingo6@gmail.com>
2022-09-04 02:43:57 +0800

    update dark mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   const [result, setResult] = useState("0");
   const [reset, setReset] = useState(false);
 
+  const toggleMode = () => {
+    let body = document.querySelector("body");
+
+    body.classList.toggle("dark");
+  };
+
   const handleClick = (e) => {
     let value = e.target.innerHTML;
     let checkValue = result.slice(-1);
@@ -57,35 +63,38 @@ function App() {
   };
 
   return (
-    <div className="calculator">
-      <div className="buttons">
-        <h2 id="value">{result}</h2>
-        <span onClick={clear} id="clear">
-          Clear
-        </span>
-        <span onClick={handleClick}>/</span>
-        <span onClick={handleClick}>*</span>
-        <span onClick={handleClick}>7</span>
-        <span onClick={handleClick}>8</span>
-        <span onClick={handleClick}>9</span>
-        <span onClick={handleClick}>-</span>
-        <span onClick={handleClick}>4</span>
-        <span onClick={handleClick}>5</span>
-        <span onClick={handleClick}>6</span>
-        <span onClick={handleClick} id="plus">
-          +
-        </span>
-        <span onClick={handleClick}>1</span>
-        <span onClick={handleClick}>2</span>
-        <span onClick={handleClick}>3</span>
-        <span onClick={handleClick}>0</span>
-        <span onClick={handleClick}>00</span>
-        <span onClick={handleClick}>.</span>
-        <span onClick={calculate} id="equal">
-          =
-        </span>
+    <>
+      <div onClick={toggleMode} className="toggleBtn"></div>
+      <div className="calculator">
+        <div className="buttons">
+          <h2 id="value">{result}</h2>
+          <span onClick={clear} id="clear">
+            Clear
+          </span>
+          <span onClick={handleClick}>/</span>
+          <span onClick={handleClick}>*</span>
+          <span onClick={handleClick}>7</span>
+          <span onClick={handleClick}>8</span>
+          <span onClick={handleClick}>9</span>
+          <span onClick={handleClick}>-</span>
+          <span onClick={handleClick}>4</span>
+          <span onClick={handleClick}>5</span>
+          <span onClick={handleClick}>6</span>
+          <span onClick={handleClick} id="plus">
+            +
+          </span>
+          <span onClick={handleClick}>1</span>
+          <span onClick={handleClick}>2</span>
+          <span onClick={handleClick}>3</span>
+          <span onClick={handleClick}>0</span>
+          <span onClick={handleClick}>00</span>
+          <span onClick={handleClick}>.</span>
+          <span onClick={calculate} id="equal">
+            =
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
